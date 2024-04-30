41b5f2a40a7d303fe38a343f084f9ff58d334083
Eric Bergstrome <eric.bergstrome@ametek.com>
2024-04-30 16:12:31 -0400

    Fix bugs

--- lisp.c
+++ lisp.c
@@ -5,40 +5,58 @@
 
 void repl(void)
 {
-    int ret, c, count, has_unmatched_close;
+    int ret, c, count, has_overflow, has_underflow, has_error;
 
-    while (1) {
+    do {
+
         fputs("> ", stdout);
+
         count = 0;
-        has_unmatched_close = 0;
-        while (1) {
+        has_overflow = 0;
+        has_underflow = 0;
+
+        do {
             ret = fgetc(stdin);
             if (ret == EOF) {
-                puts("");
-                puts("BYE");
-                return;
+                c = '\n';
             }
-            c = ret;
-            if (c == '(' && count < INT_MAX) {
-                count++;
-            }
-            if (c == ')' && count > INT_MIN) {
-                count--;
-            }
-            if (count < 0) {
-                has_unmatched_close = 1;
-            }
-            if (c == '\n') {
-                if (!has_unmatched_close && count == 0) {
-                    puts("GOOD");
+            if (ret != EOF) {
+                c = ret;
+                if (!has_underflow && !has_overflow) {
+                    if (c == '(') {
+                        if (count == INT_MAX) {
+                            has_overflow = 1;
+                        }
+                        if (count < INT_MAX) {
+                            count++;
+                        }
+                    }
+                    if (c == ')') {
+                        if (count == 0) {
+                            has_underflow = 1;
+                        }
+                        if (count > 0) {
+                            count--;
+                        }
+                    }
                 }
-                if (has_unmatched_close || count > 0) {
-                    puts("BAD");
-                }
-                break;
+            }
+        } while (c != '\n');
+
+        if (ret != EOF) {
+            has_error = has_underflow || has_overflow || count > 0;
+            if (!has_error) {
+                puts("GOOD");
+            }
+            if (has_error) {
+                puts("BAD");
             }
         }
-    }
+
+    } while (ret != EOF);
+
+    puts("");
+    puts("BYE");
 }
 
 int main(int argc, char **argv)
